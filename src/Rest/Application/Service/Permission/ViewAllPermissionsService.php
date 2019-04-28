5213f80c66b87eab32402378d467a4bde33c5cc7
AkronimBlack <bojanmadzaracka@gmail.com>
2019-04-28 12:40:59 +0200

    mark allready assigned permissions if roleId is sent

--- src/Rest/Application/Service/Permission/ViewAllPermissionsService.php
+++ src/Rest/Application/Service/Permission/ViewAllPermissionsService.php
@@ -6,20 +6,25 @@ namespace Rest\Application\Service\Permission;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Rest\Domain\Entity\Permission;
+use Rest\Domain\Entity\Role;
+use Rest\Domain\Services\Exceptions\NoSuchRoleException;
 
 class ViewAllPermissionsService
 {
     private $permissionRepository;
+
+    private $roleRepository;
 
     public function __construct(
         EntityManagerInterface $em
     )
     {
         $this->permissionRepository = $em->getRepository(Permission::class);
+        $this->roleRepository = $em->getRepository(Role::class);
     }
 
     /**
-     * @param null $request
+     * @param ViewAllPermissionsRequest $request
      *
      * @return array
      */
@@ -27,11 +32,29 @@ class ViewAllPermissionsService
     {
         $permissions = $this->permissionRepository->findAll();
         $permissionsArray = [];
+        if($request->getRoleId()){
+            $role = $this->roleRepository->find($request->getRoleId());
+            if(!$role){
+                throw new NoSuchRoleException(['role' => $request->getRoleId()]);
+            }
+        }
+
         foreach ($permissions as $permission){
+            $hasPermission = false;
+            if($request->getRoleId()){
+                foreach ($role->getPermissions() as $existingPermission){
+                    /** @var $existingPermission Permission */
+                    if($existingPermission->getName() === $permission->getName()){
+                        $hasPermission = true;
+                        break;
+                    }
+                }
+            }
             $permissionsArray[] = [
                 'name' => $permission->getName(),
                 'route' => $permission->getRoute(),
-                'type' => $permission->getType()
+                'type' => $permission->getType(),
+                'hasPermission' => $hasPermission
             ];
         }
         return $permissionsArray;
